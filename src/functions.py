872a0de2706d4211623f92281dbb90f13c1481e7
Rasmus <rasmusbamse@hotmail.com>
2018-12-18 19:55:42 +0100

    Ask for destination of TOBJ file.
    Also, you are now able to use it directly from the command prompt.

--- src/functions.py
+++ src/functions.py
@@ -8,11 +8,19 @@ def error(message):
 	print("Error - " + message)
 
 def pause_exit():
-	if sys.platform == "linux" or sys.platform == "linux2":
-		sys.exit(0)
-	elif sys.platform == "win32":
-		print("Press any key to exit the program...")
-		os.system("pause >nul")
+	if len(sys.argv) < 4:
 		sys.exit(0)
 	else:
-		sys.exit(0)
+		if sys.platform == "linux" or sys.platform == "linux2":
+			sys.exit(0)
+		elif sys.platform == "win32":
+			print("Press any key to exit the program...")
+			os.system("pause >nul")
+			sys.exit(0)
+		else:
+			sys.exit(0)
+
+def stripquotes(input):
+	input = input.replace('"', '')
+	input = input.replace("'", '')
+	return input
--- src/tobjcreator.py
+++ src/tobjcreator.py
@@ -1,15 +1,17 @@
 import binascii
 import os
 import sys
+import tempfile
 from functions import *
 
 version = "v1.1"
 
-def main():
+def main(tobjfilepath = False,path = False,tobjoutpath = False):
 	successful = False
+	selected = False
 
 	# Initialize console (Windows)
-	if sys.platform == "win32":
+	if sys.platform == "win32" and len(sys.argv) < 4:
 		os.system("cls")
 		os.system("mode con: cols=88 lines=25")
 		os.system("color 0B")
@@ -18,48 +20,81 @@ def main():
 	separator()
 
 	# Get first part of a TOBJ file.
-	while not successful:
-		print("Enter path of base TOBJ file to be used (Drag the file over the console window):")
-		tobjfilepath = input("> ")
-		separator()
-		# Get rid of junk characters from dragging a file onto the console.
-		tobjfilepath = tobjfilepath.replace('"', '')
-		tobjfilepath = tobjfilepath.replace("'", '')
+	if not tobjfilepath:
+		while not successful:
+			print("Enter path of base TOBJ file to be used (Drag the file over the console window):")
+			tobjfilepath = input("> ")
+			separator()
+			# Get rid of junk characters from dragging a file onto the console.
+			tobjfilepath = stripquotes(tobjfilepath)
+			if not os.path.exists(tobjfilepath):
+				error("File does not exist.")
+			else:
+				successful = True
+	else:
 		if not os.path.exists(tobjfilepath):
 			error("File does not exist.")
-		else:
-			successful = True
 	file = open(tobjfilepath, "rb")
 	filecontents = file.read(40)
 	file.close()
 	successful = False
 
 	# Make other part of the TOBJ file. (DDS file stuff)
-	while not successful:
-		print("Enter path you want to be in your TOBJ file:")
-		path = input("> ")
-		separator()
+	if not path:
+		while not successful:
+			print("Enter path you want to be in your TOBJ file:")
+			path = input("> ")
+			separator()
+			pathlength = len(path)
+			if pathlength > 255:
+				error("Path is too long.")
+			else:
+				successful = True
+	else:
 		pathlength = len(path)
 		if pathlength > 255:
 			error("Path is too long.")
-		else:
-			successful = True
 	pathlength_hex = hex(pathlength)[2:]
 	if len(pathlength_hex) == 1:
 		pathlength_hex = "0" + pathlength_hex
+	successful = False
+
+	# Input destination of TOBJ file.
+	if not tobjoutpath:
+		while not successful:
+			print("Enter destination of TOBJ file:")
+			tobjoutpath = input("> ")
+			separator()
+			if os.path.exists(tobjoutpath):
+				while not selected:
+					print("Destination already exists. Overwrite? [Y/N]")
+					overwrite = input("> ").lower()
+					if overwrite == "y":
+						selected = True
+						successful = True
+					if overwrite == "n":
+						selected = True
+					separator()
+				selected = False
+			else:
+				successful = True
 
 	# Write the file.
-	file = open("export.tobj", "wb")
+	file = open(tobjoutpath, "wb")
 	file.write(filecontents + binascii.unhexlify(pathlength_hex))
 	file.write(b'\x00\x00\x00\x00\x00\x00\x00')
 	file.close()
-	file = open("export.tobj", "a")
+	file = open(tobjoutpath, "a")
 	file.write(path)
 	file.close()
 
-	print("TOBJ file has been successfully created!\nIt has been saved as export.tobj in the script folder.")
-	pause_exit()
+	print("TOBJ file has been successfully created!")
+	if len(sys.argv) < 4:
+		pause_exit()
 
 
 if __name__ == '__main__':
-    main()
+	if len(sys.argv) < 4:
+		main()
+	else:
+		main(sys.argv[1],sys.argv[2],sys.argv[3])
